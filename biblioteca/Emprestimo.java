f7721972c310e6ca03b2887804f31de64aeeee01
GoldenGrace <anaflaviafiorini@gmail.com>
2026-04-16 21:30:46 -0300

    Refactor Emprestimo class with encapsulation and status
    Refactor Emprestimo class to include status and encapsulate fields.

--- biblioteca/Emprestimo.java
+++ biblioteca/Emprestimo.java
@@ -3,16 +3,61 @@ package Biblioteca;
 import java.time.*;
 
 public class Emprestimo {
-    int cpf_usuario;
-    int id_livro;
-    LocalDate emprestimo;
-    LocalDate devolucao;
+    private int cpf_usuario;
+    private int id_livro;
+    private LocalDate emprestimo;
+    private LocalDate devolucao;
 
     public Emprestimo(Usuario usuario, Livro livro){
-        this.cpf_usuario = usuario.getCpf();
-        this.id_livro = livro.getId();
-        emprestimo = LocalDate.now();
-        devolucao = emprestimo.plusDays(14);
+        setCpf_usuario(usuario.getCpf());
+        setId_livro(livro.getId());
+        setEmprestimo(LocalDate.now());
+        setDevolucao(emprestimo.plusDays(14));
+        setStatus(Status.emprestado);
+    }
+    enum Status{
+        emprestado,
+        devolvido
+    }
+    private Status status;
+
+    public int getCpf_usuario() {
+        return cpf_usuario;
     }
 
+    public int getId_livro() {
+        return id_livro;
+    }
+
+    public LocalDate getDevolucao() {
+        return devolucao;
+    }
+
+    public LocalDate getEmprestimo() {
+        return emprestimo;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setCpf_usuario(int cpf_usuario) {
+        this.cpf_usuario = cpf_usuario;
+    }
+
+    public void setDevolucao(LocalDate devolucao) {
+        this.devolucao = devolucao;
+    }
+
+    public void setEmprestimo(LocalDate emprestimo) {
+        this.emprestimo = emprestimo;
+    }
+
+    public void setId_livro(int id_livro) {
+        this.id_livro = id_livro;
+    }
 }
