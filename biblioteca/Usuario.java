589657d35a1e11b307747bfaa3847c9d6c9f4bcd
GoldenGrace <anaflaviafiorini@gmail.com>
2026-04-23 21:39:23 -0300

    Add method to retrieve user type from input
    Added a method to return user type based on input.

--- biblioteca/Usuario.java
+++ biblioteca/Usuario.java
@@ -2,6 +2,7 @@ package Biblioteca;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Usuario {
     private String nome;
@@ -57,5 +58,30 @@ public class Usuario {
 
     public void setTipo(Tipo tipo) {
         this.tipo = tipo;
+    }
+
+    public static Tipo retornarTipo(){
+        Tipo t = null;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Selecione o tipo do usuário:");
+        System.out.println("1 - Aluno");
+        System.out.println("2 - Professor");
+        System.out.println("Caso deseje cancelar a ação, digite 0");
+        int x = sc.nextInt();
+        switch (x){
+            case 0:
+                System.out.println("Cancelando a ação.");
+                break;
+            case 1:
+                t = Tipo.aluno;
+                break;
+            case 2:
+                t = Tipo.professor;
+                break;
+            default:
+                throw new IllegalArgumentException("Tipo inválido.");
+        }
+
+        return t;
     }
 }
