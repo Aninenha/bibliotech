9d3a0585a4b7a1e5e71302ff9ae5fdc27f7f9a99
GoldenGrace <anaflaviafiorini@gmail.com>
2026-04-23 21:36:49 -0300

    Refactor Categoria class and add category selection

--- biblioteca/Categoria.java
+++ biblioteca/Categoria.java
@@ -1,22 +1,22 @@
 package Biblioteca;
 
+import java.util.Scanner;
+
 public class Categoria {
     enum Nome{
         romance,
-        poesia,
-        conto,
-        cronica,
+        scifi,
+        suspense,
+        policial,
         drama,
         fabula,
-        ensaio,
+        poesia,
         biografia,
-        autobiografia,
+        trueFact,
         fantasia
     };
-    int id;
-    public Categoria(Nome nome, int id){
+    public Categoria(Nome nome){
         setNome(nome);
-        setId(id);
     }
 
     Nome nome;
@@ -25,16 +25,62 @@ public class Categoria {
         this.nome = nome;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public Nome getNome() {
         return nome;
     }
 
-    public int getId() {
-        return id;
+    public static Nome retornarCategoria(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Selecione o gênero do livro:");
+        System.out.println("1 - Romance");
+        System.out.println("2 - Ficção Científica");
+        System.out.println("3 - Suspense");
+        System.out.println("4 - Policial");
+        System.out.println("5 - Drama");
+        System.out.println("6 - Fabula");
+        System.out.println("7 - Poesia");
+        System.out.println("8 - Biografia");
+        System.out.println("9 - Baseado em eventos reais");
+        System.out.println("10 - Fantasia");
+        System.out.println("Caso deseje cancelar a ação, digite 0");
+        int x = sc.nextInt();
+        Nome n = null;
+        switch (x) {
+            case 0:
+            break;
+            case 1:
+                n = Nome.romance;
+            break;
+            case 2:
+                n = Nome.scifi;
+                break;
+            case 3:
+                n = Nome.suspense;
+                break;
+            case 4:
+                n = Nome.policial;
+                break;
+            case 5:
+                n = Nome.drama;
+                break;
+            case 6:
+                n = Nome.fabula;
+                break;
+            case 7:
+                n = Nome.poesia;
+                break;
+            case 8:
+                n = Nome.biografia;
+                break;
+            case 9:
+                n = Nome.trueFact;
+                break;
+            case 10:
+                n = Nome.fantasia;
+                break;
+            default:
+                throw new IllegalArgumentException("Valor inválido");
+        }
+        return n;
     }
 }
-
